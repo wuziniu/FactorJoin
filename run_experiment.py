c89a9fc1f19964284cf7e54452ff220d16b1508d
Ziniu Wu <ziniuw@Zinius-MacBook-Pro.local>
2022-03-03 09:14:21 -0500

    update

--- run_experiment.py
+++ run_experiment.py
@@ -2,11 +2,9 @@ import argparse
 import logging
 import os
 import time
-import shutil
-import numpy as np
-import pandas as pd
 
 from Evaluation.training import train_one_stats
+from Evaluation.testing import test_on_stats
 
 if __name__ == '__main__':
 
@@ -15,7 +13,8 @@ if __name__ == '__main__':
 
     # generate models/ensembles
     parser.add_argument('--generate_models', help='Trains BNs on dataset', action='store_true')
-    parser.add_argument('--data_path', default='/home/ubuntu/End-to-End-CardEst-Benchmark/datasets/stats_simplified/{}.csv')
+    parser.add_argument('--data_path',
+                        default='/home/ubuntu/End-to-End-CardEst-Benchmark/datasets/stats_simplified/{}.csv')
     parser.add_argument('--model_path', default='/home/ubuntu/data_CE/CE_scheme_models/')
     parser.add_argument('--n_bins', type=int, default=200, help="The bin size on the id attributes")
     parser.add_argument('--bucket_method', type=str, default="greedy", help="The bin size on the id attributes")
@@ -23,8 +22,12 @@ if __name__ == '__main__':
 
     # evaluation
     parser.add_argument('--evaluate', help='Evaluates models to compute cardinality bound', action='store_true')
-    parser.add_argument('--model_location', nargs='+', default='/home/ubuntu/data_CE/CE_scheme_models/model_stats_200.pkl')
-    parser.add_argument('--query_file_location', default='/home/ubuntu/End-to-End-CardEst-Benchmark/workloads/stats_CEB/stats_CEB.sql')
+    parser.add_argument('--model_location', nargs='+',
+                        default='/home/ubuntu/data_CE/CE_scheme_models/model_stats_greedy_200.pkl')
+    parser.add_argument('--query_file_location',
+                        default='/home/ubuntu/End-to-End-CardEst-Benchmark/workloads/stats_CEB/stats_CEB.sql')
+    parser.add_argument('--save_folder',
+                        default='/home/ubuntu/data_CE/CE_scheme_models/')
     
     # log level
     parser.add_argument('--log_level', type=int, default=logging.DEBUG)
@@ -51,5 +54,8 @@ if __name__ == '__main__':
             print(f"Training completed: total training time is {end_time - start_time}")
             
         elif args.evaluate:
+            save_file = args.save_folder + "stats_CEB_sub_queries_" + \
+                        args.model_path.split("/")[-1].split(".pkl")[0] + ".txt"
+            test_on_stats(args.model_path, args.query_file_location, save_file)
             
 
