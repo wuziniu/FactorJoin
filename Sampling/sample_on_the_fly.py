53c9c6a4ad5cb5125e8628f8c691907d30d6ea34
wuziniu <wuziniu2015@gmail.com>
2023-04-25 16:13:43 -0400

    adding sampling

--- Sampling/sample_on_the_fly.py
+++ Sampling/sample_on_the_fly.py
@@ -1,6 +1,6 @@
-import psycopg2 as pg
-from Sampling.utils.parse_sql import parse_sql
+import numpy as np
 from multiprocessing import Pool
+from Join_scheme.factor import Factor
 from Sampling.get_query_binned_cards import get_binned_sqls, exec_sql
 
 
@@ -18,4 +18,40 @@ def sample_on_the_fly(sql, table_buckets, tables_alias, ground_truth_factors_no_
     table_pdfs = dict()
     filter_size = dict()
 
-    return None
+    for i, alias in enumerate(alltabs):
+        cards = res[i][0]
+        if cards is None:
+            continue
+        column = allcols[i]
+        alias = alias[0]
+        key = tables_alias[alias] + "." + column
+        n_bins = table_buckets[tables_alias[alias]].bin_sizes[key]
+        pdfs = np.zeros(n_bins)
+        for (j, val) in cards:
+            if j is None:
+                j = 0
+            pdfs[j] += val
+        table_len = np.sum(pdfs)
+        if table_len == 0:
+            # no sample satisfy the filter, set it with a small value
+            table_len = 1
+            pdfs = ground_truth_factors_no_filter[tables_alias[alias]].pdfs[key]
+        else:
+            pdfs /= table_len
+        if alias not in table_pdfs:
+            table_pdfs[alias] = dict()
+            filter_size[alias] = table_len
+        table_pdfs[alias][key] = pdfs
+
+    for alias in tables_alias:
+        if alias in table_pdfs:
+            table_len = min(ground_truth_factors_no_filter[tables_alias[alias]].table_len,
+                            filter_size[alias]/(sampling_percentage/100))
+            na_values = ground_truth_factors_no_filter[tables_alias[alias]].na_values
+            conditional_factors[alias] = Factor(tables_alias[alias], table_len, list(table_pdfs[alias].keys()),
+                                                table_pdfs[alias], na_values=na_values)
+        else:
+            #ground-truth distribution
+            conditional_factors[alias] = ground_truth_factors_no_filter[tables_alias[alias]]
+    return conditional_factors
+
